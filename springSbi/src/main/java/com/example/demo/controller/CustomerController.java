28e991b4cf107ac37a4221147b8c9041fc9f5811
Omkar Ghugarkar <62425457+omkarghugarkar007@users.noreply.github.com>
2023-08-14 14:00:31 +0530

    Update CustomerController.java

--- springSbi/src/main/java/com/example/demo/controller/CustomerController.java
+++ springSbi/src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,12 +1,22 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
 
@@ -23,4 +33,48 @@ public class CustomerController {
 	public List<Customer> getAllCustomers(){
 		return customerRepository.findAll();
 	}
+	
+	@PostMapping("/sendCustomer")
+	public Customer createCustomer(@Validated @RequestBody Customer newCustomer)
+	{
+		return customerRepository.save(newCustomer);
+	}
+	
+	@PutMapping("/updateCustomer/{id}")
+	public ResponseEntity<Customer> updateCustomer(@PathVariable(value="id") Long userID, @Validated @RequestBody Customer newCustomer) throws ResourceNotFoundException
+	{
+		Customer updatedCustomer = customerRepository.findById(userID).orElseThrow(()-> new ResourceNotFoundException("Customer is not avaiable:"+ userID));
+		updatedCustomer.setFirstName(newCustomer.getFirstName());
+		updatedCustomer.setLastName(newCustomer.getLastName());
+		updatedCustomer.setAddress(newCustomer.getAddress());
+		updatedCustomer.setContactNo(newCustomer.getContactNo());
+		updatedCustomer.setDOB(newCustomer.getDOB());
+		customerRepository.save(updatedCustomer);
+		
+		return ResponseEntity.ok(updatedCustomer);
+	}
+	
+	@DeleteMapping("/deleteCustomer/{id}")
+	public Map<String, Boolean> deleteCustomer(@PathVariable(value="id") Long userID) throws ResourceNotFoundException {
+		Customer updatedCustomer = customerRepository.findById(userID).orElseThrow(() -> new ResourceNotFoundException("Customer is not Available:"+ userID));
+		customerRepository.delete(updatedCustomer);
+		Map<String,Boolean> response = new HashMap<>();
+		response.put("Customer has been Deleted", Boolean.TRUE);
+		return response;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
